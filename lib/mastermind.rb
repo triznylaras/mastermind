3e96a35ff86d8c9d8ee0f833bfa1e29ca2c84f52
triznylaras <triznylaras@gmail.com>
2021-10-27 15:22:48 +0700

    Initial commit

---
+++ lib/mastermind.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mastermind/version"
+
+module Mastermind
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mastermind/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mastermind
+  VERSION = "0.1.0"
+end
