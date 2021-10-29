c67b1d7540b193535ffa14c7831e038eb450c715
triznylaras <triznylaras@gmail.com>
2021-10-29 15:29:10 +0700

    add logic to get comparation between master code and guess code

---
+++ lib/mastermind/game_logic.rb
@@ -0,0 +1,36 @@
+module Mastermind
+  class GameLogic
+    def compare(master, guess)
+      cont_master = master.clone
+      cont_guess = guess.clone
+
+      @exact_number = exact_matches(cont_master, cont_guess)
+      @same_number = same_numbers(cont_master, cont_guess)
+      @total_number = exact_number + same_number
+    end
+
+    def exact_matches(master, guess)
+      exact = 0
+      master.each_with_index do |number, index|
+        next unless number == guess[index]
+
+        exact += 1
+        master[index] = '*'
+        guess[index] = '*'
+      end
+      exact
+    end
+
+    def same_numbers(master, guess)
+      same = 0
+      guess.each_index do |index|
+        next unless master.include?(guess[index]) && guess[index] != '*'
+
+        same += 1
+        master[index] = '?'
+        guess[index] = '?'
+      end
+      same
+    end
+  end
+end
