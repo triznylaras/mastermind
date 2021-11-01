b59e9e5070ef4cf61c70b6cd39aa54c452ad7a33
triznylaras <triznylaras@gmail.com>
2021-11-01 18:09:44 +0700

    add turn_messages and turn_outcome method

--- lib/mastermind/human_solver.rb
+++ lib/mastermind/human_solver.rb
@@ -1,8 +1,10 @@
-# require_relative './mastermind/text_content.rb'
-
 module Mastermind
   class HumanSolver
     include TextContent
+    include Display
+    include GameLogic
+
+    attr_reader :computer_code, :guess, :exact_number, :same_number
 
     def initialize(code_type)
       random_numbers =
@@ -24,11 +26,26 @@ module Mastermind
     def turn_order
       turn = 1
       while turn <= 12
+        turn_messages(turn)
         @guess = player_input.split(//)
         turn += 1
 
         break if guess[0].downcase == 'q'
+
+        show_code(guess)
+
+        turn_outcome
       end
+    end
+
+    def turn_messages(turn)
+      puts turn_message('guess_prompt', turn)
+      puts warning_message('last_turn') if turn == 12
+    end
+
+    def turn_outcome
+      compare(computer_code, guess)
+      show_clues(exact_number, same_number)
     end
 
     def player_input
