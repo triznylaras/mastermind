e019a1a6e2e5a011cfe2b135bc21ffa5c554bb47
triznylaras <triznylaras@gmail.com>
2021-11-02 16:40:03 +0700

    include text_content module

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -1,12 +1,13 @@
 module Mastermind
   class Game
-    include Display
+    include Mastermind::Textcontent
+    include Mastermind::Display
 
     def play
       puts instruction
       game_mode = mode_selection
       code_type = code_selection
-      code_maker if game_mode == '1' && !code_type.nil?
+      code_maker if game_mode == '1'
       code_breaker(code_type) if game_mode == '2' && !code_type.nil?
     end
 
@@ -14,21 +15,20 @@ module Mastermind
       input = gets.chomp
       return input if input.match(/^[1-2]$/)
 
-      puts("Please choose 1 or 2!")
+      puts game_message('choose')
       mode_selection
     end
 
-    # def code_maker
-      
-    # end
+    def code_maker
+      maker = ComputerSolver.new
+    end
     
     def code_selection
-      puts("Would you like to allow duplicate numbers in the code?")
-      puts("Press '1' to NOT allow duplicates\nPress '2' to allow duplicates")
+      puts game_message('allow_duplicate')
       code_type = gets.chomp
       return code_type if code_type.match(/^[1-2]$/)
 
-      puts("Please choose 1 or 2!")
+      puts game_message('choose')
       code_selection
     end
 
